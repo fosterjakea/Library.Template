fd7eee0eb9de22bee4fa3a56db1b41be64a4a4ea
Jake Foster <fosterjakea@outlook.com>
2021-07-12 20:56:13 +1000

    Update license and target frameworks

--- test/Library.Tests/CalculatorTests.cs
+++ test/Library.Tests/CalculatorTests.cs
@@ -1,5 +1,6 @@
-﻿// Copyright (c) COMPANY-PLACEHOLDER. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+﻿// Copyright (c) {companyName}.  All rights reserved.
+// Licensed under the {licenseName} license.  See {licenseFile} file in the project root for
+// full license information.
 
 using Library;
 using Xunit;
